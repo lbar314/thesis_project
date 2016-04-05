0bcb0567a24cea0ad72a99877c72c1aa3ebba842
Luca Barioglio <luca3.14@hotmail.com>
2016-04-05 11:28:17 +0200

    Bag in copyer fixed: debug -> compiler

--- copyer.sh
+++ copyer.sh
@@ -8,6 +8,6 @@ cp $HOME/thesis_project/BuildDictionary.* . || printf "Failed to copy BuildDicti
 cp $HOME/thesis_project/LookUp.* . || printf "Failed to copy Lookup.*\n"
 cp $HOME/thesis_project/FastSimulation.* . || printf "Failed to copy FastSimulation.*\n"
 cp $HOME/thesis_project/testBuild.C . || printf "Faild to copy testBuild.C\n"
-cp $HOME/thesis_project/debug.C . || printf "Failed to copy debug.C\n"
+cp $HOME/thesis_project/compiler.C . || printf "Failed to copy compiler.C\n"
 cp $HOME/thesis_project/testDecoding5.C . || printf "Failed to copy testDecoding5.C\n"
 cp $HOME/thesis_project/testSimulation.C .|| printf "Failed to copy testSimulation.C\n"
