0c102aaaa18f49ddbfe2accbb87cc624179e35d0
lbar314 <luca3.14@hotmail.com>
2016-06-26 17:44:05 +0200

    Bug fixed in FastSimulation

--- FastSimulation.cxx
+++ FastSimulation.cxx
@@ -1,15 +1,16 @@
 #include "./FastSimulation.h"
 #include <random>
 #include <algorithm>
+#include <iostream>
 
-FastSimulation::FastSimulation(std::string fname){
+FastSimulation::FastSimulation(std::string fname,unsigned seed){
   fDict.ReadBinary(fname);
+  generator = std::mt19937(seed);
+  distribution = std::uniform_real_distribution<double>(0.0,1.0);
+
 }
 
 int FastSimulation::GetRandom(){
-  double &max1 = fDict.fGroupVec.back().freq;
-  std::default_random_engine generator;
-  std::uniform_real_distribution<double> distribution(0.0,1.0);
   double rnd = distribution(generator);
   auto ind = std::upper_bound(fDict.fGroupVec.begin(),fDict.fGroupVec.end(),rnd, [] (const double &comp1, const GroupStr &comp2) {return comp1<comp2.freq;});
   return std::distance(fDict.fGroupVec.begin(),ind);
--- FastSimulation.h
+++ FastSimulation.h
@@ -1,15 +1,18 @@
 #ifndef FASTSIMULATION_H
 #define FASTSIMULATION_H
 #include "./Dictionary.h"
+#include <random>
 
 class FastSimulation{
 
   public:
-    FastSimulation(std::string fname);
+    FastSimulation(std::string fname, unsigned seed=0xdeadbeef);
     int GetRandom();
 
   private:
     Dictionary fDict;
+    std::mt19937 generator;
+    std::uniform_real_distribution<double> distribution;
 };
 
 #endif
